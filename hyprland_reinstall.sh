f3108001c93b9ab224bd495b8c7c37208eeddd9a
Robert MacCracken <robert.maccracken@gmail.com>
2026-02-15 14:33:00 -0800

    Create hyprland_reinstall.sh

---
+++ hyprland_reinstall.sh
@@ -0,0 +1,9 @@
+```bash
+# 1. Run backup reminder first
+chmod +x backup-reminder.sh
+./backup-reminder.sh
+
+# 2. Run main setup (after backing up!)
+chmod +x hyprland-workstation-setup.sh
+./hyprland-workstation-setup.sh
+```
